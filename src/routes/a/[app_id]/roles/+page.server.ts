942488e409b8de396daa6dc50b1156dccc4a4a10
Dre <bryodiiidah@gmail.com>
2023-04-12 13:13:53 +0000

    protect routes

--- src/routes/a/[app_id]/roles/+page.server.ts
+++ src/routes/a/[app_id]/roles/+page.server.ts
@@ -2,9 +2,9 @@ import { redirect } from '@sveltejs/kit';
 import type { RequestEvent } from './$types';
 
 export async function load({ locals }: RequestEvent) {
+	//@ts-ignore
 	const space = locals.space;
 	//@ts-ignore
-
 	let user: any = locals.spaceSession?.user;
 	if (user) user.role = space.roles.find((role: { id: any }) => role.id === user?.userRolesId);
 
