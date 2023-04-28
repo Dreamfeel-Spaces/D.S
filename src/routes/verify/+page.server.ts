7d70f7e0a0bb6e9e43df5ccda3233f415794f9d8
Dre <bryodiiidah@gmail.com>
2023-04-28 12:17:40 +0000

    verify users

--- src/routes/verify/+page.server.ts
+++ src/routes/verify/+page.server.ts
@@ -1,14 +1,18 @@
 import type { RequestEvent } from './$types';
 import { error, redirect } from '@sveltejs/kit';
-import {
-	CAPTCHA_SITEKEY,
-} from '$env/static/private';
+import { CAPTCHA_SITEKEY } from '$env/static/private';
+import { prisma } from '$lib/db/prisma';
 export async function load({ locals }: RequestEvent) {
 	//@ts-ignore
-	const user = locals.user;
+	const session = await locals.getSession();
 
+	if (!session) throw redirect(302, '/accounts');
 
-	if (!user) throw redirect(302, '/accounts');
+	const user = await prisma.user.findUnique({
+		where: {
+			email: session?.user?.email ?? ''
+		}
+	});
 
 	return { user, CAPTCHA_SITEKEY };
 }
